a12095acbacd2f2cd679d65b51ea738c62181e96
miclesniak <258968@student.pwr.edu.pl>
2023-05-11 18:21:36 +0200

    Sortowanie i Wyszukiwanie

--- WebApp/Pages/Beers/Index.cshtml.cs
+++ WebApp/Pages/Beers/Index.cshtml.cs
@@ -21,9 +21,11 @@ namespace WebApp.Pages.Beers
 
         public IList<Beer> Beer { get;set; } = default!;
         public string SearchString { get; set; }
+        public string CurrentSort { get; set; } 
 
-       public async Task OnGetAsync(string searchString)
+       public async Task OnGetAsync(string sortOrder, string searchString)
         {
+            CurrentSort = sortOrder;
             IQueryable<Beer> beerQuery = _context.Beer;
 
             if (!string.IsNullOrEmpty(searchString))
@@ -32,7 +34,29 @@ namespace WebApp.Pages.Beers
                 SearchString = searchString;
             }
 
-            Beer = await beerQuery.ToListAsync();
+            switch (sortOrder)
+            {
+                case "title_desc":
+                    beerQuery = beerQuery.OrderByDescending(b => b.Title);
+                    break;
+                case "releaseDate":
+                    beerQuery = beerQuery.OrderBy(b => b.RelaseDate);
+                    break;
+                case "volume":
+                    beerQuery = beerQuery.OrderBy(b => b.Volume);
+                    break;
+                case "voltage":
+                    beerQuery = beerQuery.OrderBy(b => b.Voltage);
+                    break;
+                case "price":
+                    beerQuery = beerQuery.OrderBy(b => b.Price);
+                    break;
+                default:
+                    beerQuery = beerQuery.OrderBy(b => b.Title);
+                    break;
+            }
+
+            Beer = await beerQuery.AsNoTracking().ToListAsync();
         }
     }
 }
